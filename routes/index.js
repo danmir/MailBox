c3195d9881df4026f4204235089a5c280278346a
danmir <danmir@me.com>
2015-12-03 18:20:50 +0500

    Автодополнение

--- routes/index.js
+++ routes/index.js
@@ -21,4 +21,21 @@ router.use('/send', function(req, res, next) {
     console.log(req.body);
 });
 
+// Для автодополнения
+router.use('/allusers', function(req, res, next) {
+    console.log(req.body.user);
+    var users = [];
+    db.each("SELECT user, message FROM messages WHERE user LIKE '%" + "" + req.body.user + "%'", function(err, row) {
+        users.push(row.user);
+        // console.log(row.user);
+    }, function(err, numrows) {
+        console.log(users);
+        res.writeHead(200, {"Content-Type": "application/json"});
+        var json = JSON.stringify({
+            users: users
+        });
+        res.end(json);
+    });
+});
+
 module.exports = router;
